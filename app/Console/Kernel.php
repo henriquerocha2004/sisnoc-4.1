6f271f58999b2c6d3bceb0160b61083d6e771508
rochahenrique18@gmail.com <rochahenrique18@gmail.com>
2019-12-17 20:18:33 -0300

    teste de rotina de falta de energia - 13

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('FaltaDeEnergia:cron')->everyMinute();
+        $schedule->command('FaltaDeEnergia:cron')->everyFiveMinutes();
     }
 
     /**
